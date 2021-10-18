b74eed967af43108f2b4daf27f525ea7a600e402
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2021-10-18 00:09:30 -0300

    slidesDoSENAI

---
+++ c#/estudosSENAI/estudosSENAI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace estudosSENAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Fabio", 27, 169, true);
+            int i = Pessoa.getQuantidadePessoas();
+            Console.WriteLine(i);
+        }      
+    }
+}
---
+++ c#/estudosSENAI/estudosSENAI/classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudosSENAI
+{
+    public class Pessoa
+    {
+        String nome;
+        int idade;
+        int altura;
+        bool sabeNadar;
+        private static int quantidadePessoas = 0;
+        public void andar(float velocidade)
+        {
+
+        }
+        public void nadar(float velocidade)
+        {
+
+        }
+        public Pessoa(String nome, int idade, int altura, bool sabeNadar)
+        {
+            quantidadePessoas++;
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            this.sabeNadar = sabeNadar;
+        }
+        public static int getQuantidadePessoas()
+        {
+            return quantidadePessoas;
+        }
+    }
+}
