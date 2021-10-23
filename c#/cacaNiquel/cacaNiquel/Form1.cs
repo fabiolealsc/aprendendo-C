e4a675b0df58e35981fd14e1e9df3e140f4968c0
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2021-10-23 16:51:05 -0300

    commit

---
+++ c#/cacaNiquel/cacaNiquel/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace cacaNiquel
+{
+    public partial class Form1 : Form
+    {
+        sort s = new sort();
+        teste t = new teste();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            lblN1.Text = Convert.ToString(s.sortei());
+            lblN2.Text = Convert.ToString(s.sortei());
+            lblN3.Text = Convert.ToString(s.sortei());
+            bool c = t.test(lblN1.Text, lblN2.Text, lblN3.Text);
+            if (c == true)
+            {
+                MessageBox.Show("Você Ganhou");
+            }
+        }
+
+    }
+    
+}
---
+++ c#/cacaNiquel/cacaNiquel/sort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace cacaNiquel
+{
+    class sort
+    {
+        private Random r = new Random();
+        public sort()
+        {
+           
+        }
+        public int sortei()
+        {
+            return r.Next(3);
+        }
+        
+    }
+}
+
