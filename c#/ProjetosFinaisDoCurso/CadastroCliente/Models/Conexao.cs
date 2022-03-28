2fe8f5bce524ed7b29218a10f214517ec276188f
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2022-03-28 19:52:10 -0300

    TendoAprendido

---
+++ c#/ProjetosFinaisDoCurso/CadastroCliente/Models/Conexao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CadastroCliente.Models
+{
+    public class Conexao : DbContext
+    {
+        public Conexao() : base("CadastroClientes") 
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
