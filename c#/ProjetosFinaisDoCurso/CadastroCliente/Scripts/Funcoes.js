7a415f491e018f4c97ccf4dea1ba4cd79de10c91
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2022-03-29 04:32:30 -0300

    Foi

---
+++ c#/ProjetosFinaisDoCurso/CadastroCliente/Scripts/Funcoes.js
@@ -0,0 +1,25 @@
+﻿var lista = [12, 15.6, 17, 'Fabio'];
+
+
+var objetos = [
+    {
+        nome: 'Fabio',
+        idade: 32,
+        altura: 168,
+        especialidades: ['Programação', 'Ingles', 'Banco de Dados']
+    },
+    {
+        nome: 'Luiz',
+        idade: 25,
+        altura: 135,
+        especialidades: ['Vijia', 'Perto', 'Preguiçoso']
+    }
+]
+
+function alterarParagrafo(cor, tamanho, texto, id) {
+    var paragrafo = document.getElementById(id)
+
+    paragrafo.style.color = cor;
+    paragrafo.style.fontSize = tamanho;
+    paragrafo.innerHTML = texto;
+}
