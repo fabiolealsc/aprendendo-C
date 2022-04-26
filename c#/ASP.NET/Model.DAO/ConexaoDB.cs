ac5f12fe6f6171a519936fde2c5b0dbcc1597667
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2022-04-25 23:46:02 -0300

    Foi

---
+++ c#/ASP.NET/Model.DAO/ConexaoDB.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.DAO
+{
+    internal class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private SqlConnection con;
+        private ConexaoDB() => con = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = financeiro; Integrate Security = True");
+
+        public static ConexaoDB saberEstado()
+        {
+            if (objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+            return objConexaoDB;
+        }
+        public SqlConnection getCon() => con;
+        public void CloseDB() => objConexaoDB = null;
+    }
+}
---
+++ c#/ASP.NET/Model.Entity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model.Entity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
