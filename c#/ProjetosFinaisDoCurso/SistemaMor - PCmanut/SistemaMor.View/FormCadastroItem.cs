aac4600e8e6ed9d93c5061c4fafc92df022526ae
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2022-05-01 23:55:44 -0300

    Foi

---
+++ c#/ProjetosFinaisDoCurso/SistemaMor - PCmanut/SistemaMor.View/FormCadastroItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaMor.Entidades;
+using SistemaMor.Model;
+
+namespace SistemaMor.View
+{
+    public partial class FormCadastroItem : Form
+    {
+        ItensEnt itensEnt = new ItensEnt();
+        string table;
+        public FormCadastroItem(string _table)
+        {
+            table = _table;
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                itensEnt.Descricao = txtDescricao.Text;
+                itensEnt.Item = txtItem.Text;
+                itensEnt.Imagem = imagemItem.Image;
+
+                int x = ItensModel.Inserir(itensEnt, table);
+
+                if (x > 0)
+                {
+                    MessageBox.Show(string.Format("Item {0} cadastrado(a)!", txtItem.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Não inserido!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex);
+            }
+        }
+
+        private void btnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.Filter = "img files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog()== DialogResult.OK)
+            {
+                string file = openFileDialog.FileName;
+                imagemItem.Image = Image.FromFile(file);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ c#/ProjetosFinaisDoCurso/SistemaMor - PCmanut/SistemaMor.View/FormEditarItem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaMor.Entidades;
+using SistemaMor.Model;
+
+namespace SistemaMor.View
+{
+    public partial class FormEditarItem : Form
+    {
+        ItensEnt itensEnt;
+        string table;
+        public FormEditarItem(ItensEnt _itensEnt, string _table)
+        {
+            itensEnt = _itensEnt;
+            table = _table;
+            InitializeComponent();
+            CarregarDados();
+        }
+        public void CarregarDados()
+        {
+            txtItem.Text = itensEnt.Item;
+            txtDescricao.Text = itensEnt.Descricao;
+            imagemItem.Image = itensEnt.Imagem;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                itensEnt.Descricao = txtDescricao.Text;
+                itensEnt.Item = txtItem.Text;
+                itensEnt.Imagem = imagemItem.Image;
+
+                int x = ItensModel.Editar(itensEnt, table);
+
+                if (x > 0)
+                {
+                    MessageBox.Show(string.Format("Item {0} editado(a)!", txtItem.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Não editado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex);
+            }
+        }
+
+        private void btnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.Filter = "img files (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFileDialog.FileName;
+                imagemItem.Image = Image.FromFile(file);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
