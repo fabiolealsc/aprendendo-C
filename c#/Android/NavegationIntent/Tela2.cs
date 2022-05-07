f8be4c38f15dd15bc23732fd7ce412cc49cc03a6
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2022-05-07 04:24:00 -0300

    Mais Android

---
+++ c#/Android/NavegationIntent/Tela2.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System;
+
+namespace NavegationIntent
+{
+    [Activity(Label = "Activity1")]
+    public class Tela2 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.Tela2);
+            base.OnCreate(savedInstanceState);
+
+            Button btn2 = FindViewById<Button>(Resource.Id.button2);
+            btn2.Click += Btn2_Click;
+            // Create your application here
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            this.Finish();
+        }
+        
+    }
+}
